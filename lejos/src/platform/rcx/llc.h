fde5d1d5055b34accd7cd133470d407df54967fc
lgriffiths <>
2002-06-15 15:25:46 +0000

    Added experimental support for low level comms

---
+++ lejos/src/platform/rcx/llc.h
@@ -0,0 +1,10 @@
+#ifndef _LLC_H
+#define _LLC_H
+
+extern void llc_init(void);
+extern int llc_read(void);
+extern void llc_write(unsigned char b);
+extern short llc_data_available(void);
+
+#endif
+
