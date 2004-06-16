20fc5b33d0dddedd8fafc0e5cace6a85e8d04a08
markus_heiden <>
2004-06-16 21:47:37 +0000

    reduced tabs to the needed ones

--- org.lejos.ldt.core/src/java/org/lejos/tools/eclipse/plugin/launch/RunTabGroup.java
+++ org.lejos.ldt.core/src/java/org/lejos/tools/eclipse/plugin/launch/RunTabGroup.java
@@ -2,21 +2,14 @@ package org.lejos.tools.eclipse.plugin.launch;
 
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
-import org.eclipse.debug.ui.EnvironmentTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
-import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
 
 /**
  * leJOS tab group
  */
-public class RunTabGroup
-    extends
-      AbstractLaunchConfigurationTabGroup
+public class RunTabGroup extends AbstractLaunchConfigurationTabGroup
 {
   /*
    * (non-Javadoc)
@@ -26,10 +19,7 @@ public class RunTabGroup
    */
   public void createTabs (ILaunchConfigurationDialog dialog, String mode)
   {
-    ILaunchConfigurationTab[] tabs = new ILaunchConfigurationTab[]
-    {new JavaMainTab(), new JavaArgumentsTab(), new JavaJRETab(),
-        new JavaClasspathTab(), new SourceLookupTab(), new EnvironmentTab(),
-        new CommonTab()};
-    setTabs(tabs);
+    setTabs(new ILaunchConfigurationTab[]
+    {new JavaMainTab(), new CommonTab()});
   }
 }
